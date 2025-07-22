e53ea2128230850debef1412186a30b99bd35d65
boopathirk9080 <boopathi8486@gmail.com>
2025-07-22 18:15:36 +0530

    gif edited

--- src/component/generate/Generate.jsx
+++ src/component/generate/Generate.jsx
@@ -4,6 +4,7 @@ function Generate() {
   return (
     <div className='flex flex-col items-center justify-center h-screen space-y-4'>
       <img className='  h-1/2 '  src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExMmp6MGJ6eGZlNGdkNnY2b2picnVnZzMzeXZ0cDN0NjIxZG83ajJkZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/wtw87EDOsfKhNm90nD/giphy.gif" alt="" />
+    <br />
     </div>
   )
 }
